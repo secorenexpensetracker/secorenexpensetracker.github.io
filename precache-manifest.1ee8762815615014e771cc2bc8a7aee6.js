94a9d677771d576d69c2f56ff8512e8332f5e2d5
Secoren <secorenexpensetracker@gmail.com>
2020-07-01 23:06:35 -0500

    Updates

---
+++ precache-manifest.1ee8762815615014e771cc2bc8a7aee6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9572f09cc6e83a760498419cd62007fc",
+    "url": "/secorenexpensetracker.github.io/index.html"
+  },
+  {
+    "revision": "d57a22db027acca08685",
+    "url": "/secorenexpensetracker.github.io/static/css/main.849f33f3.chunk.css"
+  },
+  {
+    "revision": "b77e9960ef79ee8e7dcb",
+    "url": "/secorenexpensetracker.github.io/static/js/2.e822b30f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/secorenexpensetracker.github.io/static/js/2.e822b30f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d57a22db027acca08685",
+    "url": "/secorenexpensetracker.github.io/static/js/main.b49be937.chunk.js"
+  },
+  {
+    "revision": "66ef6443f48b2f879a90",
+    "url": "/secorenexpensetracker.github.io/static/js/runtime-main.c3abba77.js"
+  },
+  {
+    "revision": "ee9d91d1d54e0f137aff889a323dc789",
+    "url": "/secorenexpensetracker.github.io/static/media/home-bar-graph.ee9d91d1.png"
+  },
+  {
+    "revision": "61e54be0b7c1ca5c767e8aa002603243",
+    "url": "/secorenexpensetracker.github.io/static/media/home-doughnut-graph.61e54be0.png"
+  },
+  {
+    "revision": "9da918705d8c4c50b4fef1c1c535dfbe",
+    "url": "/secorenexpensetracker.github.io/static/media/home-line-graph.9da91870.png"
+  },
+  {
+    "revision": "db5cc5c6cca7c7bc2610ffbb7e78b11f",
+    "url": "/secorenexpensetracker.github.io/static/media/home-pie-chart.db5cc5c6.png"
+  },
+  {
+    "revision": "b74c52a389a21e1fe5303f9876abe9b7",
+    "url": "/secorenexpensetracker.github.io/static/media/home-shield-image.b74c52a3.png"
+  },
+  {
+    "revision": "38433efcd078e48f8791ac0ab41c6b1d",
+    "url": "/secorenexpensetracker.github.io/static/media/howitworks1.38433efc.jpeg"
+  },
+  {
+    "revision": "f94eb759d6382fe00e2339af8f9e53f6",
+    "url": "/secorenexpensetracker.github.io/static/media/howitworks2.f94eb759.jpg"
+  },
+  {
+    "revision": "de5732ec19724a7e9e430d6f2bac738d",
+    "url": "/secorenexpensetracker.github.io/static/media/howitworks3.de5732ec.jpeg"
+  },
+  {
+    "revision": "898a4eb5c99b8e991341cba56d8e3cbe",
+    "url": "/secorenexpensetracker.github.io/static/media/mukund.898a4eb5.JPG"
+  }
+]);
